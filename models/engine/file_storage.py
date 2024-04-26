ca69626a5cad437738a4ac76ee6922667ea714d3
Matt Nyaseda <mattkeneth@gmail.com>
2024-04-26 19:00:09 +0000

    new

---
+++ models/engine/file_storage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class FileStorage:
+
+    __file_path = 'file.json'
+    __objects = []
+
+    def all(self):
+        return cls.__objects
+
+    def new(self, obj):
+
+    def save(self):
+
+    def reload(self):
+
