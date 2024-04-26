3e3c6f4d17e45479ea6cbcbed58f465460427fc2
Matt Nyaseda <mattkeneth@gmail.com>
2024-04-26 11:52:59 +0000

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -13,10 +13,17 @@ class BaseModel:
     / methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args. **kwargs):
         """
         Instatiates an object with it' s attributes
         """
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
